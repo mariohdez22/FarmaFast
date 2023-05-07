25e654094e5b0e79573eb1a454987bc457fcf238
Josue2704 <josue30759@gmail.com>
2023-05-07 14:37:51 -0600

    crud

---
+++ Models/ViewModels/ProductosVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FarmaFast.Models.ViewModels
+{
+    public class ProductosVM
+    {
+        public Producto producto { get; set; }
+        
+        public List<SelectListItem> listaTipo { get; set; }
+    }
+}
